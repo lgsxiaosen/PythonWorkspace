270125fa6ba7d4af227ccf2d8a1e53983a131912
fuck off to study <lgsdaredevil@users.noreply.github.com>
2017-10-09 14:03:19 +0800

    Add files via upload

---
+++ Test2/set_list.py
@@ -0,0 +1,25 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    gsliu 2017-05-19
+    
+"""
+
+
+gss = {'name': 'lala', 'age': 12, 'afd': 'qq', 'tyuds': 456}
+
+ff = set(gss.keys())
+print ff
+if 'r' in ff:
+    print 1
+# for i in ff:
+#     print i
+#     print gss[i]
+
+
+
+
+
+
+
+
