f233509d9f7e4c071c758a011623641771cc6060
fuck off to study <lgsdaredevil@users.noreply.github.com>
2017-10-09 14:07:39 +0800

    Add files via upload

---
+++ Test5/test.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    gsliu 2017-09-11
+    
+"""
+
+
+a = 'abc_oi_9'
+
+b = a.split("_")
+
+print b
+print b[-1]
+
+
+
+
+
+
---
+++ Test5/test1.py
@@ -0,0 +1,19 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    gsliu 2017-09-27
+    
+"""
+
+
+a = None
+if a:
+    for b in a:
+        print 'a'
+print 'b'
+
+
+
+
+
+
