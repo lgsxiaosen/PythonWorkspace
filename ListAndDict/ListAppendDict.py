1575d6601b9455fead3b4210dc3dadac3c66a28b
gsliu <gsliu@travelsky.com>
2018-02-05 10:02:41 +0800

    add changes

---
+++ ListAndDict/ListAppendDict.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    gsliu 
+    @create 2018-02-05 9:04
+"""
+
+list1 = []
+
+for i in range(1, 5):
+    dict1 = {"name": "ad", "sch": 123}
+    dict1['num'] = i
+    list1.append(dict1)
+print list1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
